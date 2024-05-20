8fcfc72244ad340be3913e4e1de3e4e95391f568
Archangel <jesse@archangel.wtf>
2024-05-20 16:51:21 +0200

    Initial Commit

---
+++ PatchSuperSampling.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Aki.Reflection.Patching;
+using Comfort.Common;
+using EFT;
+
+namespace UnflashbangHideout
+{
+    internal class PatchSuperSampling : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod() => typeof(CameraClass).GetMethod("SetSuperSampling", BindingFlags.Public | BindingFlags.Instance);
+
+        [PatchPrefix]
+        private static bool PatchPrefix(float sampling)
+        {
+            try
+            {
+                var Gameworld = Singleton<GameWorld>.Instance;
+
+                if (Gameworld.MainPlayer.Location == "hideout")
+                {
+                    Logger.LogInfo("Overriding the usage of SuperSampling in the hideout");
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Logger.LogInfo($"Caught Exception: {ex}");
+
+                return true;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,13 @@
+﻿using BepInEx;
+
+namespace UnflashbangHideout
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, "Un-flashbang Hideout", "1.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            new PatchSuperSampling().Enable();
+        }
+    }
+}
